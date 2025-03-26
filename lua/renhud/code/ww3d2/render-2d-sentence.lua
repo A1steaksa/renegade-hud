e5835d36577aaf92367d81fc7b9e12119d3e6249
John Schneider <8369707+A1steaksa@users.noreply.github.com>
2025-03-26 14:53:33 -0500

    Renamed "LIB" to "STATIC"

--- lua/renhud/code/ww3d2/render-2d-sentence.lua
+++ lua/renhud/code/ww3d2/render-2d-sentence.lua
@@ -1,44 +1,44 @@
 -- Based on Code/ww3d2/render2dsentence.cpp
 
----@class Renegade
----@field Render2DSentence Render2DSentence
+--- @class Renegade
+--- @field Render2DSentence Render2DSentence
 
 CNC_RENEGADE.Render2DSentence = CNC_RENEGADE.Render2DSentence or {}
 
----@class Render2DSentence
-local LIB = CNC_RENEGADE.Render2DSentence
+--- @class Render2DSentence
+local STATIC = CNC_RENEGADE.Render2DSentence
 
 --#region Surface Get Font
 
----The currently active Surface font
----@type string
-SURFACE_FONT = ""
+--- The currently active Surface font
+--- @type string
+local surfaceFont = ""
 
 local oldSetFont = surface.SetFont
 
 
----@param fontName string
+--- @param fontName string
 function surface.SetFont( fontName )
-    SURFACE_FONT = fontName
+    surfaceFont = fontName
     oldSetFont( fontName )
 end
 
----Retrives the font that is currently active in the Surface library
----@return string # The currently active font name
+--- Retrives the font that is currently active in the Surface library
+--- @return string # The currently active font name
 function surface.GetFont()
-    return SURFACE_FONT
+    return surfaceFont
 end
 --#endregion
 
----Format is (Font Name) -> table< String: string, { Width: integer, Height: integer } >
----@type table<string, table< string, { Width: integer, Height: integer } > >
+--- Format is (Font Name) -> table< String: string, { Width: integer, Height: integer } >
+--- @type table<string, table< string, { Width: integer, Height: integer } > >
 local perFontSizeCache = {}
 
----Determines the width, in pixels, of a given character
----@param str string
----@return integer
-function LIB.GetCharWidth( str )
-    local font = SURFACE_FONT
+--- Determines the width, in pixels, of a given character
+--- @param str string
+--- @return integer
+function STATIC.GetCharWidth( str )
+    local font = surfaceFont
 
     local fontCache = perFontSizeCache[ font ]
     if not fontCache then
@@ -59,22 +59,22 @@ function LIB.GetCharWidth( str )
     return charCache.Width
 end
 
----Determines the width, in pixels, of a given character
----@param char string
-function LIB.GetCharSpacing( char )
-    local width = LIB.GetCharWidth( char )
+--- Determines the width, in pixels, of a given character
+--- @param char string
+function STATIC.GetCharSpacing( char )
+    local width = STATIC.GetCharWidth( char )
 
     -- Non-zero width characters get an extra pixel of width for some reason
     -- Presumably someone on the Renegade development team thought the letters looked too close together
     return width == 0 and 0 or width + 1
 end
 
----Draws a piece of text using Renegade's abnormal text rendering
----@param x number
----@param y number
----@param text string
----@param color Color
-function LIB.DrawText( x, y, text, color )
+--- Draws a piece of text using Renegade's abnormal text rendering
+--- @param x number
+--- @param y number
+--- @param text string
+--- @param color Color
+function STATIC.DrawText( x, y, text, color )
     surface.SetTextColor( color )
 
     local currentX = x + 0
@@ -87,20 +87,20 @@ function LIB.DrawText( x, y, text, color )
         surface.SetTextPos( currentX, currentY )
         surface.DrawText( char )
 
-        currentX = currentX + LIB.GetCharSpacing( char )
+        currentX = currentX + STATIC.GetCharSpacing( char )
     end
 end
 
----Determines the width, in pixels, of a string of text that uses Renegade's abonrmal text rendering
----@param text string
----@return number Width
-function LIB.GetTextWidth( text )
+--- Determines the width, in pixels, of a string of text that uses Renegade's abonrmal text rendering
+--- @param text string
+--- @return number Width
+function STATIC.GetTextWidth( text )
     local currentWidth = -0.5
 
     local chars = string.Explode( "", text )
 
     for _, char in ipairs( chars ) do
-        currentWidth = currentWidth + LIB.GetCharSpacing( char )
+        currentWidth = currentWidth + STATIC.GetCharSpacing( char )
     end
 
     return currentWidth
