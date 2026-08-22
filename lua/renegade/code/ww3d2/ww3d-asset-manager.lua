ae6f82d4d04446ecaba0776adfa2fdc12f3640d0
John Schneider <8369707+A1steaksa@users.noreply.github.com>
2026-08-22 11:38:55 -0500

    Implement more model loading code

--- lua/renegade/code/ww3d2/ww3d-asset-manager.lua
+++ lua/renegade/code/ww3d2/ww3d-asset-manager.lua
@@ -22,15 +22,6 @@ INSTANCE.IsWw3dAssetManager = true
 
 --#region Imports
 
-	--- @type RenderObjectClass
-	local renderObjectClass = CNC.Import( "code/ww3d2/render-object.lua" )
-
-	--- @type MeshClass
-	local meshClass = CNC.Import( "code/ww3d2/mesh.lua" )
-
-	--- @type MeshModelClass
-	local meshModelClass = CNC.Import( "code/ww3d2/mesh-model.lua" )
-
 	--- @type PrototypeClass
 	local prototypeClass = CNC.Import( "code/ww3d2/prototype.lua" )
 
@@ -215,7 +206,7 @@ function INSTANCE:Load3dAssets( fileName )
 		local file = fileFactoryClass.TheFileFactory:GetFile( fileName )
 		if file then
 			if file:IsAvailable() then
-				result = self:Load3dAssets( file )
+				result = INSTANCE.Load3dAssets( self, file )
 			end
 			fileFactoryClass.TheFileFactory:ReturnFile( file )
 		end
@@ -236,24 +227,20 @@ function INSTANCE:Load3dAssets( fileName )
 			local chunkId = cload:CurChunkId()
 
 			if chunkId == w3dFileIds.W3D_CHUNK_TYPE.W3D_CHUNK_HIERARCHY then
-				section.Warn( "Skipping loading asset chunk for HTreeManager" )
-				-- STATIC.HTreeManager:LoadTree( cload )
-
+				self.HTreeManager:LoadTree( cload )
 			elseif (
 				   chunkId == w3dFileIds.W3D_CHUNK_TYPE.W3D_CHUNK_ANIMATION
 				or chunkId == w3dFileIds.W3D_CHUNK_TYPE.W3D_CHUNK_COMPRESSED_ANIMATION
 				or chunkId == w3dFileIds.W3D_CHUNK_TYPE.W3D_CHUNK_MORPH_ANIMATION
 			) then
-				section.Warn( "Skipping loading asset chunk for HAnimManager" )
-				-- STATIC.HAnimManager:LoadAnim( cload )
-
+				self.HAnimationManager:LoadAnimation( cload )
 			else
-				self:LoadPrototype( cload )
+
+				INSTANCE.LoadPrototype( self, cload )
 			end
 
 			cload:CloseChunk()
 		end
-
 		w3dFile:Close()
 
 		return true
@@ -269,17 +256,15 @@ function INSTANCE:ReleaseUnusedAssets()
 end
 
 --- "Create me an instance of one of the prototype render objects"
---- @param connectedEntity Entity
 --- @param name string
---- @param sourceModelPath string
 --- @return RenderObjectInstance?
-function INSTANCE:CreateRenderObject( connectedEntity, name, sourceModelPath )
+function INSTANCE:CreateRenderObject( name )
 	-- "Try to find a prototype"
 	local prototype = self:FindPrototype( name )
 
 	-- "If we didn't find one, try to load on demand"
 	if self.Ww3dLoadOnDemand and prototype == nil then
-		section.Start( "Loading Render Object Prototype on demand for: ", name )
+		section.Start( "Loading Render Object Prototype on demand for: '", name, "'" )
 
 		local fileName
 		local periodIndex = textUtils.IndexOf( name, "." )
@@ -302,6 +287,7 @@ function INSTANCE:CreateRenderObject( connectedEntity, name, sourceModelPath )
 	end
 
 	if prototype == nil then
+		section.Warn( "Failed to find or create prototype for '", name, "'" )
 		return -- "Failed to find a prototype"
 	end
 
@@ -625,25 +611,23 @@ end
 function INSTANCE:LoadPrototype( cload )
 	-- "Get the chunk id"
 	local chunkId = cload:CurChunkId()
-	local chunkIdName = table.KeyFromValue( w3dFileIds.W3D_CHUNK_TYPE, chunkId )
-	local chunkIdNameString = ( chunkIdName ~= nil and " (" .. chunkIdName .. ")" or nil )
 
 	-- "Find a loader that handles that type of chunk"
 	local loader = self:FindPrototypeLoader( chunkId )
 	local newPrototype
 
-	section.Start( "Loading Chunk ID ", chunkId, chunkIdNameString )
-
 	if loader ~= nil then
 		-- "Ask it to create a prototype from the contents of the chunk."
 		newPrototype = loader:LoadW3d( cload )
-
-		section.End()
 	else
+		local chunkIdName = table.KeyFromValue( w3dFileIds.W3D_CHUNK_TYPE, chunkId )
+		local chunkIdNameString = ( chunkIdName ~= nil and " (" .. chunkIdName .. ")" or nil )
+		local classNameString = ( loader ~= nil and " " .. loader.Class or nil )
 		section.Error(
 			"Failed to find prototype loader for Chunk ID ",
 			chunkId,
-			chunkIdNameString
+			chunkIdNameString,
+			classNameString
 		)
 		return false
 	end
